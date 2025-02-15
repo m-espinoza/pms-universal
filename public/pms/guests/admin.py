9358c37ec32a708eafc47ea0ce60fdf9904b4a5d
mespinoza <espinoza.manuel1989@gmail.com>
2025-02-15 18:55:46 -0300

    add: agregado de guest al panel de admins

--- public/pms/guests/admin.py
+++ public/pms/guests/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Guest  # Importa el modelo
 
-# Register your models here.
+# Registra el modelo en el panel de administración
+admin.site.register(Guest)
