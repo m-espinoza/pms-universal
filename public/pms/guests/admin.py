c3772bdeee4423b383193d02ef1167c5288b57b3
mespinoza <espinoza.manuel1989@gmail.com>
2025-02-18 00:24:41 -0300

    add: enhance Guest admin interface with improved display and filtering options

--- public/pms/guests/admin.py
+++ public/pms/guests/admin.py
@@ -1,5 +1,16 @@
 from django.contrib import admin
-from .models import Guest  # Importa el modelo
+from .models import Guest
 
-# Registra el modelo en el panel de administración
-admin.site.register(Guest)
+@admin.register(Guest)
+class GuestAdmin(admin.ModelAdmin):
+    list_display = ('name', 'phone_number', 'email', 'nationality', 'created_at')
+    list_filter = ('name', 'nationality')
+    search_fields = ('name', 'phone_number', 'email', 'nationality')
+    date_hierarchy = 'created_at'
+    ordering = ('created_at',)
+    fieldsets = (
+        (None, {
+            'fields': ('name', 'phone_number ', 'email', 'nationality')
+        }),
+    )
+    readonly_fields = ('created_at', 'updated_at')
