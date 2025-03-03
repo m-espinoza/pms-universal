7e1d50cfee3478f38a1c0d8ff1f8b9599ee1beab
mespinoza <espinoza.manuel1989@gmail.com>
2025-03-03 02:20:37 -0300

    Add accounts app to settings and configure URLs for login redirection

--- public/pms/pms/settings.py
+++ public/pms/pms/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'guests',
     'bookings',
     'payments',
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -59,7 +60,7 @@ ROOT_URLCONF = 'pms.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -124,6 +125,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
