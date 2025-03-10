1d98056fbe21c8fa004035ce586597d21552815b
mespinoza <espinoza.manuel1989@gmail.com>
2025-03-09 22:16:28 -0300

    Update login and logout URLs to support authenticated redirection and HTTP methods

--- public/pms/accounts/urls.py
+++ public/pms/accounts/urls.py
@@ -3,7 +3,10 @@ from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
-    path('login/', auth_views.LoginView.as_view(template_name='accounts/login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
+    path('', auth_views.LoginView.as_view(
+        template_name='accounts/login.html',
+        redirect_authenticated_user=True
+    ), name='login'), # noqa
+    path('logout/', auth_views.LogoutView.as_view(next_page='login', http_method_names=['post', 'get']), name='logout'), # noqa
     path('dashboard/', views.dashboard, name='dashboard'),
 ]
