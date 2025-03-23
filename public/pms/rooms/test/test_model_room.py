4fdc63414d3a4e40f0005717057a561e533c0de3
mespinoza <espinoza.manuel1989@gmail.com>
2025-03-23 19:25:38 -0300

    Update room type in tests to use 'PRIVATE_ROOM' for consistency

--- public/pms/rooms/test/test_model_room.py
+++ public/pms/rooms/test/test_model_room.py
@@ -9,7 +9,7 @@ class RoomModelTest(TestCase):
         # Configuración común para las pruebas
         self.room = Room.objects.create(
             name="Room 101",
-            room_type="PRIVATE",
+            room_type="PRIVATE_ROOM",
             capacity=2,
             description="A nice private room.",
         )
@@ -22,7 +22,7 @@ class RoomModelTest(TestCase):
 
     def test_room_name_unique(self):
         with self.assertRaises(ValidationError):
-            room = Room(name="Room 101", room_type="PRIVATE")
+            room = Room(name="Room 101", room_type="PRIVATE_ROOM")
             room.full_clean()
 
     def test_modify_room(self):
