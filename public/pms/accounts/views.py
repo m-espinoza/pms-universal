e29424d870d7886be13be9e6ef91c6f118001973
mespinoza <espinoza.manuel1989@gmail.com>
2025-05-09 00:53:36 -0300

    Add user and group serializers, and implement user and group viewsets with authentication

--- public/pms/accounts/views.py
+++ public/pms/accounts/views.py
@@ -1,7 +1,45 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
+from django.contrib.auth.models import User, Group
+from rest_framework import viewsets, permissions
+from rest_framework.authtoken.serializers import AuthTokenSerializer
+from .serializers import UserSerializer, GroupSerializer
+from rest_framework.response import Response
+
+from knox.views import LoginView as KnoxLoginView
+from knox.models import AuthToken
 
 
 @login_required
 def dashboard(request):
     return render(request, "accounts/dashboard.html")
+
+
+class UserViewSet(viewsets.ModelViewSet):
+	"""
+	API endpoint that allows users to be viewed or edited.
+	"""
+	queryset = User.objects.all().order_by('-date_joined')
+	serializer_class = UserSerializer
+	permission_classes = [permissions.IsAdminUser]
+      
+class GroupViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows groups to be viewed or edited.
+    """
+    queryset = Group.objects.all().order_by('name')
+    serializer_class = GroupSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+class LoginAPI(KnoxLoginView):
+    permission_classes = (permissions.AllowAny,)
+
+    def post(self, request, format=None):
+        serializer = AuthTokenSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data['user']
+        token = AuthToken.objects.create(user)[1]
+        return Response({
+            "user": UserSerializer(user).data,
+            "token": token
+        })
--- public/pms/pms/urls.py
+++ public/pms/pms/urls.py
@@ -1,10 +1,21 @@
 from django.contrib import admin
 from django.urls import include, path
 from django.views.generic import RedirectView  # noqa
+from django.conf import settings
+from rest_framework import routers
+from rooms.views import RoomViewSet
+from accounts.views import UserViewSet, LoginAPI
 
-from accounts import urls  # noqa
+
+
+router = routers.DefaultRouter()
+router.register(r'rooms', RoomViewSet, basename='room')
+router.register(r'users', UserViewSet, basename='user')
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("accounts.urls")),
+    path('api/', include(router.urls)),
+    path('api/auth/', include('knox.urls')),
+    path('api/auth/login/', LoginAPI.as_view(), name='knox_login'),
 ]
