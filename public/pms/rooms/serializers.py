7b5b54ff9510f824ce714b2e9ad2b789d869ac3c
mespinoza <espinoza.manuel1989@gmail.com>
2025-05-09 00:54:19 -0300

    Add RoomSerializer and RoomViewSet for room management API

---
+++ public/pms/rooms/serializers.py
@@ -0,0 +1,18 @@
+from dataclasses import field
+from rest_framework import serializers
+from .models import Room
+
+class RoomSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Room
+        fields = ['id', 'property', 'name', 'room_type', 'capacity', 'base_price', 'is_active', 'description']  # Especifica los campos necesarios
+        extra_kwargs = {
+            'property_name': {'required': True},
+            'room_type': {'required': True},
+            'room_number': {'required': True},
+            'price_per_night': {'required': True},
+            'availability_status': {'required': True},
+            'description': {'required': False},  # Este campo es opcional
+        }
+
+
--- public/pms/rooms/views.py
+++ public/pms/rooms/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render  # noqa
 
-# Create your views here.
+from rest_framework import viewsets, permissions
+from .serializers import RoomSerializer
+from .models import Room
+from rest_framework.response import Response
+
+
+
+     
+class RoomViewSet(viewsets.ModelViewSet):   
+    """
+    API endpoint that allows rooms to be viewed or edited.
+    """
+    queryset = Room.objects.all()
+    serializer_class = RoomSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def list(self, request, *args, **kwargs):
+        queryset = self.get_queryset()
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
